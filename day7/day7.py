95f92de69281edb585a93f6e56c5cc24e0d8b016
Godkayaki <daniplcm@gmail.com>
2021-12-07 10:04:58 +0100

    day7

---
+++ day7/day7.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#coding=utf-8
+#Created on 06 dec. 2021 - by Daniel González Martínez
+#day 6.py
+
+import os, sys
+
+#part 1
+with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+    lines, fuel_lower = f.readlines(), 0
+    for index, line in enumerate(lines):
+        start_vals = list(map(int, line.split(',')))
+        max_v, min_v = max(start_vals), min(start_vals)
+
+    for i in range(min_v, max_v):
+        new_vals = []
+        for number in start_vals:
+            if number > i: new_vals.append(number-i)
+            else:  new_vals.append(i-number)
+        
+        if sum(new_vals) < fuel_lower or fuel_lower == 0:
+            fuel_lower = sum(new_vals)
+    
+print(fuel_lower)
+
+#part 2
+with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+    lines, fuel_lower = f.readlines(), 0
+    for index, line in enumerate(lines):
+        start_vals = list(map(int, line.split(',')))
+        max_v, min_v = max(start_vals), min(start_vals)
+
+    for i in range(min_v, max_v):
+        new_vals = []
+        for number in start_vals:
+            if number > i: 
+                t = 0
+                for x in range(1, (number-i)+1): t += x
+                new_vals.append(t)
+            else:
+                t = 0
+                for x in range(1, (i-number)+1): t += x
+                new_vals.append(t)
+        
+        if sum(new_vals) < fuel_lower or fuel_lower == 0:
+            fuel_lower = sum(new_vals)
+    
+print(fuel_lower)
