e9838999ed5c42fc78a460c6d68c102f4825736a
Godkayaki <daniplcm@gmail.com>
2021-12-09 12:59:43 +0100

    day 3 is going to be finished soon or later

--- day3/day3.py
+++ day3/day3.py
@@ -5,12 +5,12 @@
 
 #i'll be late on this i know
 
-import os, sys
+import os, sys, copy
 
 #part1
 with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
     lines, gamma, epsilon = f.readlines(), "", ""
-    for index, line in enumerate(lines[0]):
+    for index, line in enumerate(lines[0].rstrip("\n")):
         zeros, ones = 0, 0
         for index_lines, line_lines in enumerate(lines):
             if line_lines[index] != "\n":
@@ -23,8 +23,35 @@ with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
             gamma = gamma + "1"
             epsilon = epsilon + "0"
     g, e = int(gamma, 2), int(epsilon, 2)
-    print(g*e)
+
+#print(g*e)
 
 #part 2
 ogr = 0
 csr = 0
+with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+    lines, gamma, epsilon, most_c_list, less_c_list = f.readlines(), "", "", [], []
+    for index_l, line_vals in enumerate(lines):
+        most_c_list.append(line_vals.rstrip("\n"))
+        less_c_list.append(line_vals.rstrip("\n"))
+    
+    for index, line in enumerate(lines[0].rstrip("\n")):
+        vals = []
+        for index_lines, line_lines in enumerate(lines):
+            vals.append(line_lines[index])
+        most_common, less_common = max(vals, key = vals.count), min(vals, key = vals.count)
+        
+        i=0
+        for i, line_lines_r in enumerate(lines):
+            linea_tww = line_lines_r.rstrip("\n")
+            if linea_tww[index] == most_common: 
+                try:
+                    if len(less_c_list) != 1: less_c_list.remove(linea_tww)
+                except: pass
+            elif linea_tww[index] == less_common: 
+                try:
+                    if len(most_c_list) != 1: most_c_list.remove(linea_tww)
+                except: pass
+            i+=1
+        
+print(most_c_list, less_c_list)
