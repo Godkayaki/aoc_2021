b060816b980a07adaaa149e2116ba7a4a2a62f41
Godkayaki <daniplcm@gmail.com>
2021-12-09 11:35:30 +0100

    day 9 part 1

---
+++ day9/day9.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#coding=utf-8
+#Created on 09 dec. 2021 - by Daniel González Martínez
+#day 9.py
+
+#this is a fucking mess but i don't care anymore honestly
+
+import os, sys
+
+#part 1
+with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+    lines, lines_vals, res = f.readlines(), [], []
+    for index, line in enumerate(lines):
+        lines_vals.append(list(line.rstrip("\n")))
+
+    for index, line in enumerate(lines_vals):
+        up, down = None, None
+        if index == 0: up = False
+        elif index == len(lines_vals): down = False
+        
+        for index_line, val in enumerate(line):
+            left, right, vals_tocalc = None, None, []
+            if index_line == 0: left = False
+            elif index_line == len(line): right = False
+
+            #set vals
+            try: 
+                if left != False: vals_tocalc.append(int(line[index_line-1]))
+            except: pass
+            try: 
+                if up != False: vals_tocalc.append(int(lines_vals[index-1][index_line]))
+            except: pass
+            try: 
+                if right != False: vals_tocalc.append(int(line[index_line+1]))
+            except: pass
+            try: 
+                if down != False: vals_tocalc.append(int(lines_vals[index+1][index_line]))
+            except: pass
+
+            a = True
+            for num in vals_tocalc: 
+                if int(val) >= num: a = False
+            if a != False: res.append(int(val)+1)
+
+            #if left > int(val) and up > int(val) and right > int(val) and down > int(val): res.append(int(val)+1)
+    
+    print(sum(res))
