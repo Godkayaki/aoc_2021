8ab615b4f90b40b61566fe91e97597cb282e7ea1
Godkayaki <daniplcm@gmail.com>
2021-12-10 12:06:08 +0100

    day 10

--- day10/day10.py
+++ day10/day10.py
@@ -5,15 +5,55 @@
 
 import os, sys, copy
 
-#part1
-#openList, closeList = ["[", "{", "(", "<"], ["]", "}", ")", ">"]
+#part 1
+lines, res = [] , 0
 with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
-    lines = f.readlines()
-    ups = ['(', '{', '[', "<", ')', '}', ']', ">"]
-    for index, line in enumerate(lines):
-        counters = [0, 0, 0, 0, 0, 0, 0, 0]
-        real_line = line.rstrip("\n")
-        for i in real_line:
-            ind = ups.index(i)
-            counters[ind] += 1
-        print(counters)
+    for line in f:
+        lines.append(line.rstrip("\n"))
+
+#dictionaries with key-value
+type_, value = { "(": ")", "{": "}", "[": "]", "<": ">" }, { ")": 3, "]": 57, "}": 1197, ">": 25137 }
+
+for line in lines:
+    st = []
+    for index in line:
+        if index in type_: st.append(type_[index])
+        elif index == st[-1]: st.pop()
+        else:
+            res += value[index]
+
+print(res)
+
+#part 2
+lines, res = [] , 0
+with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+    for line in f:
+        lines.append(line.rstrip("\n"))
+
+#dictionaries with key-value
+type_, value = { "(": ")", "{": "}", "[": "]", "<": ">" }, { ")": 1, "]": 2, "}": 3, ">": 4 }
+
+okay_lines, scores = [], []
+for line in lines:
+    st, ok_l = [], True
+    for index in line:
+        if index in type_: st.append(type_[index])
+        elif index == st[-1]: st.pop()
+        else:
+            ok_l = False
+            break
+    
+    if ok_l != False: okay_lines.append(line)
+
+for line in okay_lines:
+    st, single_score = [], 0
+    for index in line:
+        if index in type_: st.append(type_[index])
+        elif index == st[-1]: st.pop()
+    
+    for el in reversed(st):
+        single_score = single_score * 5 + value[el]
+    scores.append(single_score)
+
+scores.sort()
+print(scores[int((len(scores)/2))])
